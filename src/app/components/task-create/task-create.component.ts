d898722835392d0e853925a247d8f37c75bbf7b8
Eimantas <eim.kasp@gmail.com>
2021-11-15 19:16:07 +0200

    Forms Validation Example

--- src/app/components/task-create/task-create.component.ts
+++ src/app/components/task-create/task-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/interfaces/task';
 import { TaskService } from 'src/app/services/task.service';
 import { switchMap, tap } from 'rxjs/operators';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-task-create',
@@ -27,15 +28,24 @@ export class TaskCreateComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  taskCreate() {
-    console.log("form submit works");
-    console.log(this.task);
-    this._taskService.createTask(this.task)
+  taskCreate(form : NgForm) {
+
+    // console.log(form.controls.author.invalid);
+
+    /* Patikriname ar visi formos laukeliai atitinka validacijos taisykles */
+    if(form.valid) {
+      this._taskService.createTask(this.task)
       .subscribe((data : any ) => {
         alert("Task succesfully added!");
-        console.log(data);
+        // Funkcija kuri nustato tuscias reiksmes formos laukeliams
+        form.resetForm();
         this.newItemEvent.emit(data);
       });
+    } else {
+      // Atvaizduojame klaida ar pranesima, vartotojui, kad forma uzpildyta neteisingai
+      alert("Please check your form data");
+    }
+
   }
 
 }
